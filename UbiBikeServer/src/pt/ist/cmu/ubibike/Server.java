58985cec99aaa8eb27ca07526dd3de214bccb88a
Pedro Joaquim <pedrommjoaquim@gmail.com>
2016-03-09 00:25:12 +0000

    starting to develop backend server

---
+++ UbiBikeServer/src/pt/ist/cmu/ubibike/Server.java
@@ -0,0 +1,23 @@
+package pt.ist.cmu.ubibike;
+
+
+public class Server {
+
+    private void run() {
+
+
+
+    }
+
+
+    /**
+     * @param args args passed in execution
+     */
+    public static void main(String[] args) {
+
+        Server server = new Server();
+        server.run();
+
+    }
+
+}
